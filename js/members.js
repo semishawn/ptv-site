3cb6ea707e90ed30fc569c49c1e1d1de8ac90551
semishawn <45698497+semishawn@users.noreply.github.com>
2021-12-02 10:49:01 -0500

    scroll animate + member dump

---
+++ js/members.js
@@ -0,0 +1,102 @@
+members = [
+	{
+		name: "Kyleigh Duff",
+		title: "Executive Producer",
+		grade: "senior"
+	},
+	{
+		name: "Zoe Streeter",
+		title: "Executive Director",
+		grade: "junior"
+	},
+	{
+		name: "Ken Carpenetti",
+		title: "Digital Media Director",
+		grade: "junior"
+	},
+	{
+		name: "Sean Nekich",
+		title: "Equipment Manager",
+		grade: "senior"
+	},
+	{
+		name: "Maya Halma",
+		title: "Studio Manager",
+		grade: "senior"
+	},
+	{
+		name: "Amanda Cohan",
+		title: "Producer",
+		grade: "junior"
+	},
+	{
+		name: "Maya Cole",
+		title: "Producer",
+		grade: "senior"
+	},
+	{
+		name: "Cloie Hummel",
+		title: "Producer",
+		grade: "Junior"
+	},
+	{
+		name: "Alex Goetz-Morales",
+		title: "Producer",
+		grade: "junior"
+	},
+	{
+		name: "Vito Rizzotto",
+		title: "Producer",
+		grade: "junior"
+	},
+	{
+		name: "Maya Sukanick",
+		title: "Producer",
+		grade: "senior"
+	},
+	{
+		name: "Cat Anderson",
+		title: "Associate Producer",
+		grade: "senior"
+	},
+	{
+		name: "Maeve Leonzi",
+		title: "Associate Producer",
+		grade: "senior"
+	},
+	{
+		name: "Sam Massad",
+		title: "Associate Producer",
+		grade: "senior"
+	},
+	{
+		name: "Alondra Reyes",
+		title: "Associate Producer",
+		grade: "junior"
+	},
+	{
+		name: "Klaudia Salvino",
+		title: "Associate Producer",
+		grade: "junior"
+	},
+	{
+		name: "Luis Sanchez",
+		title: "Associate Producer",
+		grade: "sophomore"
+	},
+	{
+		name: "Meredith Schmoyer",
+		title: "Associate Producer",
+		grade: "sophomore"
+	},
+	{
+		name: "Caleigh Sechler",
+		title: "Associate Producer",
+		grade: "sophomore"
+	},
+	{
+		name: "John Greth",
+		title: "Advisor",
+		grade: "faculty"
+	}
+];
