a397671270ff97f443c05b3e650a18253a7e6a6a
semishawn <45698497+semishawn@users.noreply.github.com>
2021-12-01 22:46:11 -0500

    fix

--- main.js
+++ main.js
@@ -1,15 +1,10 @@
+// Scroll to top on page load
 window.scrollTo(0, 0);
 
+// On scroll past 100px, shrink nav
 $(window).scroll(function() {
 	if ($(window).scrollTop() > 100)
 		$(".nav").addClass("shrink");
 	else
 		$(".nav").removeClass("shrink");
 });
-
-/* $(window).scroll(function() {  
-	if ($(window).scrollTop() >= $(".header").offset().top)
-		$(".header").addClass('sticky');
-	else
-		$(".header").removeClass('sticky');
-}); */
